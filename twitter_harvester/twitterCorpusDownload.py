0b2a72cbf098dbb9859f5fe6ec9faeffa26b4500
mamarik <81158285+mamarik@users.noreply.github.com>
2021-05-21 02:33:59 +1000

    Add files via upload
    download tweet corpus given city, start date, end date, n (every n tweet to DL - to ensure not downloading everything). can be run from command line as (for eg)
    python3 twitterCorpusDownload.py adelaide 1/1/2020 30/4/2021 10000

---
+++ twitter_harvester/twitterCorpusDownload.py
@@ -0,0 +1,57 @@
+import requests
+import process
+import json
+import couchDatabase
+import sys
+
+
+def request_twitter_corpus(city, date_start="1/1/2020", date_end="30/4/2021", every_nth_tweet=100):
+    # create tweet processor
+    processor = process.Processor(city)
+
+    # connect to database
+    our_couch = couchDatabase.CouchDatabase()
+
+    # create variables for request to twitter corpus database
+    auth = ('readonly', 'cainaimeeshaLu4Lejoo9ooW4jiopeid')
+    url = 'http://couchdb.socmedia.bigtwitter.cloud.edu.au/twitter/_design/twitter/_view/summary'
+
+    date_st = date_start.split("/")
+    date_end = date_end.split("/")
+
+    params = {
+        'reduce': 'false',
+        'include_docs': 'true',
+        'start_key': '[\"{}\", {}, {}, {}]'.format(city,date_st[2],date_st[1],date_st[0]),
+        'end_key': '[\"{}\", {}, {}, {}]'.format(city,date_end[2],date_end[1],date_end[0]),
+    }
+
+    with requests.get(url, auth=auth, params=params, stream=True) as r:
+        r.raise_for_status()
+        # create counter so we only process every nth tweet (otherwise takes too long)
+        counter = 0
+        for chunk in r.iter_content(chunk_size=None):
+            try:
+                tweet = json.loads(chunk.decode("utf-8").strip().strip(','))['doc']
+                # only process and add tweet if i) has coordinates or ii) is every 20th tweet
+                # this is to ensure we can finish in time
+                if tweet['coordinates'] is not None or counter % int(every_nth_tweet) == 0:
+                    tweet_dict = processor.processTweet(tweet)
+                    our_couch.add_tweet(tweet_dict, city)
+                counter += 1
+            except json.decoder.JSONDecodeError:
+                # could not parse (usually will be first or last line)
+                pass
+
+
+# if this script is run from command line as python city date1 date2 n
+# we want it to run the corpus download
+if __name__ == '__main__':
+    try:
+        city = sys.argv[1]
+        date1 = sys.argv[2]
+        date2 = sys.argv[3]
+        n = sys.argv[4]
+        request_twitter_corpus(city, date_start=date1, date_end=date2, every_nth_tweet=n)
+    except IndexError:
+        pass
