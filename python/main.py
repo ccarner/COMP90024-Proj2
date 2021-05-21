7bf9a96553757d7a808302ddbe220eb6b64b03ea
mamarik <81158285+mamarik@users.noreply.github.com>
2021-05-21 22:59:19 +1000

    Update main.py

---
+++ python/main.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, './twitter_harvester')
+import twitterCorpusDownload
+import twitterStreamer
+
+if __name__ == '__main__':
+    try:
+        city = sys.argv[1]  # which city to analyse
+        twitterCorpusDownload.request_twitter_corpus(city)
+        twitterStreamer.runStreamer(city)
+        # also have to add a line for harvester - incomplete!
+        # also add analysis data here too!
+    except IndexError:
+        print("Not enough arguments provided!")
