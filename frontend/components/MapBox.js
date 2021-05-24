c044f66d2025be8141210dd067879a709b2b6692
bing xu <bingxu.au@gmail.com>
2021-05-24 21:44:26 +1000

    refactored - moved maplayers into separate component

--- frontend/components/MapBox.js
+++ frontend/components/MapBox.js
@@ -6,50 +6,7 @@ import MapGL, {FlyToInterpolator, Source, Layer} from 'react-map-gl';
 import ControlPanel from "./ControlPanel";
 import {updateData, decimalYearToMonthAndWeek} from "../utils/helpers";
 import styles from "../styles/MapBox.module.css";
-
-const suburbLayer = {
-  id: 'suburbs_data',
-  type: 'line',
-  source: 'suburbs',
-  paint: {
-    "line-color": "#22B0C0",
-    "line-width": 0.5,
-    "line-opacity": 1
-  }
-};
-
-const stateLayer = {
-  id: 'state_data',
-  type: 'line',
-  source: 'states',
-  paint: {
-    "line-color": "#22B0C0",
-    "line-width": 0.5,
-    "line-opacity": 1
-  }
-};
-
-const dataLayer = {
-  id: 'data',
-  type: 'fill',
-  paint: {
-    'fill-color': {
-      property: 'sentiment',
-      stops: [
-        [0, '#3288bd'],
-        [0.04, '#d53e4f'],
-        [0.08, '#f46d43'],
-        [0.12, '#fdae61'],
-        [0.16, '#fee08b'],
-        [0.20, '#ffffbf'],
-        [0.24, '#e6f598'],
-        [0.28, '#abdda4'],
-        [0.32, '#66c2a5']
-      ]
-    },
-    'fill-opacity': 0.8
-  }
-};
+import {stateLayer, suburbLayer, dataLayer} from "./MapStyles";
 
 
 export default function MapBox({suburbData, cityData, suburbOn, activateSuburbs}) {
---
+++ frontend/components/MapStyles.js
@@ -0,0 +1,43 @@
+export const suburbLayer = {
+    id: 'suburbs_data',
+    type: 'line',
+    source: 'suburbs',
+    paint: {
+      "line-color": "#22B0C0",
+      "line-width": 0.5,
+      "line-opacity": 1
+    }
+  };
+  
+export const stateLayer = {
+    id: 'state_data',
+    type: 'line',
+    source: 'states',
+    paint: {
+      "line-color": "#22B0C0",
+      "line-width": 0.5,
+      "line-opacity": 1
+    }
+  };
+  
+export const dataLayer = {
+    id: 'data',
+    type: 'fill',
+    paint: {
+      'fill-color': {
+        property: 'sentiment',
+        stops: [
+          [0, '#3288bd'],
+          [0.04, '#d53e4f'],
+          [0.08, '#f46d43'],
+          [0.12, '#fdae61'],
+          [0.16, '#fee08b'],
+          [0.20, '#ffffbf'],
+          [0.24, '#e6f598'],
+          [0.28, '#abdda4'],
+          [0.32, '#66c2a5']
+        ]
+      },
+      'fill-opacity': 0.3
+    }
+  };
