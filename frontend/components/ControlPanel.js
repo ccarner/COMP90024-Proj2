a0cc5ed74d23fcfd1703812b7cc4790bf3626e86
bing xu <bingxu.au@gmail.com>
2021-05-25 08:06:15 +1000

    fix how dates are displayed in the control panel

--- frontend/components/ControlPanel.js
+++ frontend/components/ControlPanel.js
@@ -1,17 +1,21 @@
 
 import * as React from 'react';
 import styles from "../styles/ControlPanel.module.css";
-import {decimalYearToDateStr} from "../utils/helpers";
+import {decimalYearToDateStr, getDateOfISOWeek, decimalYearToMonthAndWeek} from "../utils/helpers";
 
 function ControlPanel(props) {
   const {year} = props;
+  const [curr_year, week_no] = decimalYearToMonthAndWeek(year);
+  const startDate = getDateOfISOWeek(week_no, curr_year);
+  const endDate = new Date(startDate);
+  endDate.setDate(endDate.getDate() + 7);
 
   return (
     <div className={styles.controlpanel}>
       <h3>Interactive GeoJSON</h3>
       <p>
-        Map showing sentiment by state in <b>{decimalYearToDateStr(year)}</b>. Hover over a state to
-        see details.
+        Map showing sentiment by state in <b>{decimalYearToDateStr(year)}, {startDate.toLocaleDateString()} - {endDate.toLocaleDateString()}</b>. Hover over a state to
+        see details. 
       </p>
       <p>
         Data source: <a href="https://aurin.org.au/">AURIN</a>
@@ -23,7 +27,7 @@ function ControlPanel(props) {
         <input
           type="range"
           value={year}
-          min={2020.02}
+          min={2020}
           max={2021.28}
           step={1/52}
           onChange={evt => props.onChange(evt.target.value)}
