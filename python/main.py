1524b617120e7d1018682d5216b3eceb83254902
mamarik <81158285+mamarik@users.noreply.github.com>
2021-05-25 14:36:29 +1000

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,14 +1,21 @@
-import sys
-# sys.path.insert(0, './twitter_harvester')
 from twitter_harvester import twitterCorpusDownload
 from twitter_harvester import twitterStreamer
+from twitter_harvester import scrape_tweets
+from analysis import analyse_aurin_sentiment
 
 if __name__ == '__main__':
     try:
         city = sys.argv[1]  # which city to analyse
-        twitterCorpusDownload.request_twitter_corpus(city)
-        twitterStreamer.runStreamer(city)
-        # also have to add a line for harvester - incomplete!
-        # also add analysis data here too!
+        task = sys.argv[2] # what script to run out of: streamer, corpus download
+        if task == "streamer":
+        	twitterStreamer.runStreamer(city)
+        elif task == "corpus":
+        	twitterCorpusDownload.request_twitter_corpus(city)
+        	if city == 'melbourne':
+        		scrape_tweets.main()
+        elif task == "analysis":
+        	analyse_aurin_sentiment.main(city)
+        else:
+        	print("invalid task!")
     except IndexError:
         print("Not enough arguments provided!")
